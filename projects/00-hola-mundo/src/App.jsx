ce615df54c8d45ce139c1d2354c77b47f40958cd
Gabo-Dev <52703336+Gabo-Dev@users.noreply.github.com>
2024-06-07 18:40:08 +0200

    Update: Estilos

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import {TwitterFollowCard} from './TwitterFollowCard.jsx'
+
+export function App () {
+    return(
+        <>
+        <TwitterFollowCard userName="Gabo-Dev" name="Jonathan G.Orna Ponce"/>
+        <TwitterFollowCard userName="Ramso93" name="Don Osmar"/>
+        </>
+    )
+}
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,23 @@
+export function TwitterFollowCard({userName, name, isFollowing}){
+    return (
+        <article className='tw-followCard'>
+                <header className='tw-followCard-header'>
+                    <img 
+                        // src evalua toda la expresión,
+                        // también podríamos usar una const y que la evalue
+                        src={`https://unavatar.io/github/${userName}`} 
+                        alt= {`El avatar de ${userName}`} 
+                        className='tw-followCard-avatar' />
+                    <div className='tw-followCard-info'>
+                        <strong>{name}</strong>
+                        <span className='tw-followCard-infoUsername'>@{userName}</span>
+                    </div>
+                </header>
+                <aside>
+                    <button className='tw-followCard-button'>
+                        Seguir
+                    </button>
+                </aside>
+            </article>
+    )
+}
